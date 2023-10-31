4d2112cb43a99e6ba0953d8dce391184cbef1131
Kshitij Kurandwad <kshitij@fundexpertfintech.com>
2023-10-31 16:22:07 +0530

    fix ecosystem

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  apps: [
-    {
-      name: 'my-react-app',
-      script: 'npx',
-      args: 'serve -s dist -l 3000',
-      instances: 'max',
-      exec_mode: 'cluster',
-      watch: false,
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-      },
-    },
-  ],
+	apps: [
+		{
+			name: "my-react-app",
+			script: "serve",
+			args: ['dist','3000'],
+			instances: 'max',
+			exec_mode: "cluster",
+			watch: false,
+			env: {
+				NODE_ENV: "production",
+				PORT: 3000,
+			},
+		},
+	],
 };
