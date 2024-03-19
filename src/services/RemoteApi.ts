b07987c03c4b384b0561e6355beb367b07e987a0
Kshitij Kurandwad <kshitij@fundexpertfintech.com>
2024-03-19 11:05:42 +0530

    Added env

--- src/services/RemoteApi.ts
+++ src/services/RemoteApi.ts
@@ -7,12 +7,7 @@ import { router } from "expo-router";
 
 // Create a new Axios instance with defaults
 const axiosInstance = axios.create({
-
-    // baseURL: "https://qvision-be.kotaksecurities.online/",
-    baseURL: "https://vision-connect.azurewebsites.net/",
-
-//     baseURL: process.env.API_ENDPOINT,
-
+    baseURL: process.env.API_ENDPOINT,
 });
 
 // Set up a response interceptor to handle errors
