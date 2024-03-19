cbba69a725fd471d309d70a42beffb000f48821a
Kshitij Kurandwad <kshitij@fundexpertfintech.com>
2024-03-19 15:44:50 +0530

    env fix

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -1,3 +1,7 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 module.exports = {
     script: "serve",
     env: {
@@ -5,5 +9,6 @@ module.exports = {
         PM2_SERVE_PORT: 3000,
         PM2_SERVE_SPA: "true",
         PM2_SERVE_HOMEPAGE: "/index.html",
+        API_ENDPOINT:process.env.API_ENDPOINT,
     },
 };
